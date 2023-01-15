313471e89390dd65e5dd514cfd1a10bb1a27d970
David <dreypad2605@iesmartinezm.es>
2023-01-15 12:19:07 +0100

    12-01

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '019603f62a519867c8a5fb7da6e0aab1',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '019603f62a519867c8a5fb7da6e0aab1',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
 
 /*
